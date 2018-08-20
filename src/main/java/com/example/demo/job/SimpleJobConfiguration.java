31d7adf8881128881961d34a43af3abd4db581ac
jw <test@test.com>
2018-08-21 08:48:15 +0900

    Job Parameter 적용

--- src/main/java/com/example/demo/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration.java
@@ -3,8 +3,10 @@ package com.example.demo.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,15 +27,29 @@ public class SimpleJobConfiguration {
 	@Bean
 	public Job simpleJob() {
 		return jobBuilderFactory.get("simpleJob") // simpleJob이란 이름의 Batch Job생성. job의 이름은 별도로 지정하지 않고 builder를 통해 지정.
-				.start(simpleStep1())
+				.start(simpleStep1(null))
+				.next(simpleStep2(null))
 				.build();
 	}
 	
 	@Bean
-	public Step simpleStep1() {
+	@JobScope
+	public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
 		return stepBuilderFactory.get("simpleStep1") // simpleStep1이라는 이름의 Batch Step 생성. builder를 통해 이름 지정.
 				.tasklet((contribution, chunkContext) -> { // Step안에서 수행될 기능 명시. Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용.
 					log.info(">>>>> This is Step1"); // Batch가 수행되면 This is Step1 출력.
+					log.info(">>>>> requestDate = {}", requestDate); // Job Parameter로 받은 값을 로그에 출력.
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+	
+	@Bean
+	@JobScope
+	public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+		return stepBuilderFactory.get("simpleStep2")
+				.tasklet((contribution, chunkContext) -> {
+					log.info(">>>>> This is Step2"); // Batch가 수행되면 This is Step1 출력.
+					log.info(">>>>> requestDate = {}", requestDate); // Job Parameter로 받은 값을 로그에 출력.
 					return RepeatStatus.FINISHED;
 				}).build();
 	}

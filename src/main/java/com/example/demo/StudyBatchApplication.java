98156d12c2787a5dcce90c2e2dd67904cbbb1cd8
jw <test@test.com>
2018-08-20 18:44:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/StudyBatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBatchApplication.class, args);
+	}
+}
